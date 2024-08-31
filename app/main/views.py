2ee1ee28378e6a7793ba0469c45c36efa41e9c9c
NfSilveira <nicolasfreitas.silveira@hotmail.com>
2024-08-31 00:02:48 +0000

    Prova Semestral

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,19 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import Flask, flash, render_template, session, redirect, url_for, current_app
 from .. import db
-from ..models import User, Role
+from ..models import User, Role, Teacher, Discipline
 from ..email import sendgrid_send_message
 from . import main
-from .forms import NameForm
+from .forms import NameForm, TeacherForm
+from datetime import datetime
+from flask_moment import Moment
+
+app = Flask(__name__)
+moment = Moment(app)
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    """form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
@@ -52,4 +57,74 @@ def index():
 
     return render_template('index.html',
                            form=form, name=session.get('name'),
-                           known=session.get('known', False), users=users, roles=roles)
+                           known=session.get('known', False), users=users, roles=roles)"""
+
+    return render_template('index.html', current_time=datetime.utcnow())
+
+
+@main.route('/professores', methods=['GET', 'POST'])
+def teachers_page():
+
+    discipline_choices = dict([
+        ('dswa5', 'DSWA5'),
+        ('gpsa5', 'GPSA5'),
+        ('ihca5', 'IHCA5'),
+        ('soda5', 'SODA5'),
+        ('pjia5', 'PJIA5'),
+        ('tcoa5', 'TCOA5'),
+    ])
+
+    teacher_registering_form = TeacherForm()
+    if teacher_registering_form.validate_on_submit():
+
+        teacher_name=teacher_registering_form.teacherName.data
+
+        teacher = Teacher.query.filter_by(teacher_name=teacher_name).first()
+        discipline = teacher_registering_form.discipline.data
+
+        print(f"Teacher Name: {teacher_name}", flush=True)
+        print(f"Discipline Name: {discipline}", flush=True)
+
+        if teacher is None:
+
+            teacher_discipline = Discipline.query.filter_by(name=discipline).first()
+
+            if teacher_discipline is None:
+                teacher_discipline = Discipline(name=discipline)
+                db.session.add(teacher_discipline)
+                db.session.commit()
+                print(f"New Discipline Created: {teacher_discipline.name}", flush=True)
+
+            print(f"Discipline Assigned: {teacher_discipline.name}", flush=True)
+
+            teacher_discipline = Discipline.query.filter_by(name=discipline).first()
+
+            teacher = Teacher(
+                teacher_name=teacher_registering_form.teacherName.data,
+                discipline_id=teacher_discipline.id  # Ensure this is set correctly
+            )
+            db.session.add(teacher)
+            db.session.commit()
+
+            flash('Professor cadastrado com sucesso!', 'success')
+
+        else:
+
+            flash('Professor já existe na base de dados!', 'success')
+
+    teachers = Teacher.query.all()
+    disciplines = Discipline.query.all()
+
+    return render_template('teachers_page.html', form=teacher_registering_form, teachers=teachers, disciplines=disciplines, discipline_choices=discipline_choices)
+
+
+@main.route('/disciplinas', methods=['GET'])
+def disciplines_page():
+
+    return render_template('whoops.html')
+
+
+@main.route('/alunos', methods=['GET'])
+def students_page():
+
+    return render_template('whoops.html')
--- app/models.py
+++ app/models.py
@@ -19,3 +19,26 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Discipline(db.Model):
+
+    __tablename__ = 'disciplines'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    teachers = db.relationship('Teacher', back_populates='discipline')
+
+    def __repr__(self):
+        return '<Discipline %r>' % self.name
+
+
+class Teacher(db.Model):
+
+    __tablename__ = 'teachers'
+    id = db.Column(db.Integer, primary_key=True)
+    teacher_name = db.Column(db.String(64), unique=True, index=True)
+    discipline_id = db.Column(db.Integer, db.ForeignKey('disciplines.id'))
+    discipline = db.relationship('Discipline', back_populates='teachers')
+
+    def __repr__(self):
+        return '<Teacher %r>' % self.teacher_name
